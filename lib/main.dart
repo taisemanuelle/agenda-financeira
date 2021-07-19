78776c44fa18c68808081667589bf55cca88c47e
taisemanuelle <emmearaujo@gmail.com>
2021-07-18 22:11:47 -0300

    criando splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,126 +1,15 @@
+import 'package:agenda_app/screen/InicialPage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'modelo/despesa.dart';
-import 'widgets/novaDespesa.dart';
-import 'widgets/listaDespesa.dart';
-import 'widgets/grafico.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:splashscreen/splashscreen.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-      .then((_) {
-    runApp(new MyApp());
+
+main() {
+  initializeDateFormatting().then((_) {
+    runApp(MaterialApp(
+      home: InicialPage(),
+      debugShowCheckedModeBanner: false,
+    ));
   });
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Agenda Financeira ',
-      home: MyHomePage(),
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-        accentColor: Colors.white,
-        fontFamily: 'Poppins',
-        appBarTheme: AppBarTheme(
-          textTheme: ThemeData.light().textTheme.copyWith(
-                headline6: TextStyle(
-                  fontFamily: 'Poppins',
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-              ),
-        ),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final List<Despesa> _userDespesas = [];
-
-  void _addNovaDespesa({String titulo, double valor, DateTime data}) {
-    final novaDespesa = Despesa(
-      id: DateTime.now().toString(),
-      titulo: titulo,
-      valor: valor,
-      data: data,
-    );
-
-    setState(() {
-      _userDespesas.add(novaDespesa);
-    });
-  }
-
-  void _inicioAddNovaDespesa(BuildContext context) {
-    showModalBottomSheet(
-      context: context,
-      builder: (_) => NovaDespesa(
-        addDespesa: _addNovaDespesa,
-      ),
-      isScrollControlled: true,
-    );
-  }
-
-  void _deletarDespesa(String id) {
-    setState(() {
-      _userDespesas.removeWhere((despesa) => despesa.id == id);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    AppBar appbar = AppBar(
-      title: Text('Agenda Financeira'),
-      actions: [
-        IconButton(
-          icon: Icon(Icons.add),
-          onPressed: () => _inicioAddNovaDespesa(context),
-        ),
-      ],
-    );
-
-    return Scaffold(
-      appBar: appbar,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: (MediaQuery.of(context).size.height -
-                      appbar.preferredSize.height -
-                      MediaQuery.of(context).padding.top) *
-                  0.5,
-              child: GraficoDeBarras(
-                despesas: _userDespesas,
-              ),
-            ),
-            Container(
-              height: (MediaQuery.of(context).size.height -
-                      appbar.preferredSize.height -
-                      MediaQuery.of(context).padding.top) *
-                  0.5,
-              child: ListaDespesa(
-                despesas: _userDespesas.reversed.toList(),
-                excluiDespesa: _deletarDespesa,
-              ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _inicioAddNovaDespesa(context),
-        child: Icon(
-          Icons.add,
-          color: Theme.of(context).primaryColor,
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
-    );
-  }
 }
--- lib/widgets/CardMovimentacoesItem.dart
+++ lib/widgets/CardMovimentacoesItem.dart
@@ -1,94 +1,199 @@
-import 'package:agenda_app/Helper/transferencias_helper.dart';
-import 'package:agenda_app/Helper/transferencias_helper.dart';
+import 'package:agenda_app/Helper/Movimentacoes_helper.dart';
 import 'package:flutter/material.dart';
 
-class TimeLineItem extends StatelessWidget {
-  final Transferencias mov;
-  final bool isLast;
-  final Color colorItem;
+import 'CustomDialog.dart';
 
-  const TimeLineItem({Key key, this.mov, this.isLast, this.colorItem})
+class CardMovimentacoesItem extends StatelessWidget {
+  final Movimentacoes mov;
+  final bool lastItem;
+
+  const CardMovimentacoesItem({Key key, this.mov, this.lastItem = false})
       : super(key: key);
+
+  _dialogConfimacao(BuildContext context, double width) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+              title: Text(
+                "Remover Movimentação",
+                textAlign: TextAlign.start,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, color: Colors.lightBlue[700]),
+              ),
+              backgroundColor: Colors.white,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(width * 0.050)),
+              content: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text("${mov.descricao}",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: width * 0.045,
+                            color: mov.tipo.toString() == "r"
+                                ? Colors.green[600]
+                                : Colors.red[600])),
+                    Text("R\$ ${mov.valor}",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: mov.tipo.toString() == "r"
+                                ? Colors.green[600]
+                                : Colors.red[600])),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Divider(
+                      color: Colors.grey[400],
+                      height: 2,
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: Text(
+                            "Cancelar",
+                            style: TextStyle(color: Colors.red[700]),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            MovimentacoesHelper movHelper =
+                                MovimentacoesHelper();
+                            movHelper.deleteMovimentacao(mov);
+                            Navigator.pop(context);
+                          },
+                          child: Container(
+                            padding: EdgeInsets.only(
+                                top: width * 0.02,
+                                bottom: width * 0.02,
+                                left: width * 0.03,
+                                right: width * 0.03),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              color: Colors.red[700],
+                            ),
+                            child: Center(
+                              child: Text(
+                                "Confirmar",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: width * 0.04),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ));
+        });
+  }
+
+  _dialogEdit(BuildContext context, double width, Movimentacoes movimentacao) {
+    print(movimentacao.toString());
+    showDialog(
+        context: context,
+        builder: (context) {
+          return CustomDialog(
+            mov: movimentacao,
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
-    double height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
 
-    return Container(
-      width: width,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    width: width * 0.03,
-                    height: height * 0.015,
-                    decoration: BoxDecoration(
-                        color: colorItem, //Colors.red[900],
-                        borderRadius: BorderRadius.circular(10)),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(
-                        top: width * 0.02, bottom: width * 0.02),
-                    child: Container(
-                      width: width * 0.004,
-                      height: isLast != true ? height * 0.05 : height * 0.03,
+    return Column(
+      children: <Widget>[
+        GestureDetector(
+          onTap: () {
+            _dialogEdit(context, width, mov);
+            //_dialogConfimacao(context, width);
+          },
+          child: Container(
+            //padding: EdgeInsets.all(width * 0.005),
+            width: width,
+            height: height * 0.08,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Container(
                       decoration: BoxDecoration(
-                        color: Colors.grey[850],
-                      ),
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(15),
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.grey[300],
+                                blurRadius: 10,
+                                offset: Offset(2, 3))
+                          ]),
+                      child: Padding(
+                          padding: EdgeInsets.all(width * 0.03),
+                          child: mov.tipo == "r"
+                              ? Icon(
+                                  Icons.arrow_downward,
+                                  color: Colors.green,
+                                  size: width * 0.06,
+                                )
+                              : Icon(Icons.arrow_upward,
+                                  color: Colors.red, size: width * 0.06)),
                     ),
-                  )
-                ],
-              ),
-              Column(
-                //mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(left: width * 0.05),
-                    child: Text(
-                      mov.descricao,
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: width * 0.05,
-                      ),
-                    ),
+                    Padding(
+                        padding: EdgeInsets.only(left: width * 0.03),
+                        child: Container(
+                          width: width * 0.4,
+                          child: Text(
+                            mov.descricao,
+                            overflow: TextOverflow.ellipsis,
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              color: mov.tipo == "r"
+                                  ? Colors.green[700]
+                                  : Colors.red[700],
+                              fontWeight: FontWeight.bold,
+                              fontSize: width * 0.044,
+                            ),
+                          ),
+                        )),
+                  ],
+                ),
+                Text(
+                  mov.tipo == "r" ? "+ ${mov.valor}" : " ${mov.valor}",
+                  style: TextStyle(
+                    color:
+                        mov.tipo == "r" ? Colors.green[700] : Colors.red[700],
+                    fontWeight: FontWeight.bold,
+                    fontSize: width * 0.044,
                   ),
-                  Padding(
-                    padding:
-                        EdgeInsets.only(left: width * 0.05, top: width * 0.02),
-                    child: Text(
-                      mov.data,
-                      style: TextStyle(
-                        color: Colors.white70,
-                        fontSize: width * 0.034,
-                      ),
-                    ),
-                  ),
-                ],
-              )
-            ],
-          ),
-          Padding(
-            padding: EdgeInsets.only(right: width * 0.05),
-            child: Text(
-              mov.tipo == "r" ? "+ ${mov.valor}" : " ${mov.valor}",
-              textAlign: TextAlign.end,
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: width * 0.04,
-                  fontWeight: FontWeight.bold),
+                ),
+              ],
             ),
-          )
-        ],
-      ),
+          ),
+        ),
+        lastItem == true
+            ? Container(
+                height: 80,
+              )
+            : Container()
+      ],
     );
   }
 }
